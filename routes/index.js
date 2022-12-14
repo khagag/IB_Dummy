1df560465cb72c0f2f1028a589f46e7084204867
kareem hagag <kareem.m.hagag@gmail.com>
2022-12-14 15:19:07 +0200

    create index post route with ack

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  res.send('<Ack xmlns=" http://www.egyptianbanks.com/ipn/schema/" api="PayResp" msgId="41d5fc5c-9b5f-4e1f-b444-df0124f0b765" ts="2019-12-31 13:12:12.123" result="SUCCESS" respCode="00000" respDesc="SUCCESS"/>');
+});
+
 module.exports = router;
